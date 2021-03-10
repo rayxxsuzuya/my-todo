50ff0b7afde37d8bd65928ceae62ed101241a999
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2021-03-10 23:54:16 +0300

    todo done

---
+++ script.js
@@ -0,0 +1,59 @@
+const $input = document.querySelector('#text');
+const $block = document.querySelector('.block');
+
+$input.addEventListener('keydown', addItem);
+
+let todos;
+
+function toLocal() {
+  todos = $block.innerHTML;
+  localStorage.setItem('todos', todos);
+}
+
+document.body.addEventListener('click', (el) => {
+  if (el.target.classList.contains('task-complete')) {
+    el.preventDefault();
+    el.target.parentElement.children[0].classList.add('completed');
+    toLocal();
+  } else if (el.target.classList.contains('task-delete')) {
+      el.target.parentElement.remove();
+      toLocal();
+  }
+});
+
+function addItem(e) {
+  if (e.key === 'Enter' && e.target.value) {
+    let item = document.createElement('div');
+    let taskText = document.createElement('p');
+    taskText.classList.add('task-text');
+    taskText.innerHTML = e.target.value;
+    item.append(taskText);
+    let taskComplete = document.createElement('button');
+    taskComplete.classList.add('fas','fa-check', 'task-complete');
+    taskComplete.addEventListener('click', completeItem);
+    item.append(taskComplete);
+    let taskDelete = document.createElement('button');
+    taskDelete.classList.add('fas','fa-trash-alt', 'task-delete');
+    taskDelete.addEventListener('click', removeItem);
+    item.append(taskDelete);
+    item.classList.add('item');
+    $block.insertAdjacentElement('beforeend', item);
+    e.target.value = '';
+    toLocal();
+  }
+} 
+
+function completeItem(e) {
+  e.preventDefault();
+  e.target.parentElement.children[0].classList.add('completed');
+  toLocal();
+}
+
+function removeItem(e) {
+  e.target.parentElement.remove();
+  toLocal();
+}
+
+if (localStorage.getItem('todos')) {
+  $block.innerHTML = localStorage.getItem('todos');
+}
